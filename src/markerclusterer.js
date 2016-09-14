4d0c7af3dac3739ef65cce4554359eca2b9cf0f1
Stephen Farrar <stephenfarrar@users.noreply.github.com>
2016-09-15 00:32:42 +1000

    Replace the OverlayView-based clusters with Markers. The Markers draw their contents in a canvas, and then convert it to a data URL.

--- src/markerclusterer.js
+++ src/markerclusterer.js
@@ -61,6 +61,7 @@
  *       'backgroundPosition': (string) The position of the backgound x, y.
  *       'iconAnchor': (Array) The anchor position of the icon x, y.
  * @constructor
+ * // TODO(step): I'm pretty sure this doesn't need to extend OverlayView anymore.
  * @extends google.maps.OverlayView
  */
 function MarkerClusterer(map, opt_markers, opt_options) {
@@ -119,20 +120,6 @@ function MarkerClusterer(map, opt_markers, opt_options) {
   this.styles_ = options['styles'] || [];
 
   /**
-   * @type {string}
-   * @private
-   */
-  this.imagePath_ = options['imagePath'] ||
-      this.MARKER_CLUSTER_IMAGE_PATH_;
-
-  /**
-   * @type {string}
-   * @private
-   */
-  this.imageExtension_ = options['imageExtension'] ||
-      this.MARKER_CLUSTER_IMAGE_EXTENSION_;
-
-  /**
    * @type {boolean}
    * @private
    */
@@ -164,16 +151,14 @@ function MarkerClusterer(map, opt_markers, opt_options) {
 
   // Add the map event listeners
   var that = this;
-  google.maps.event.addListener(this.map_, 'zoom_changed', function() {
+  google.maps.event.addListener(this.map_, 'idle', function() {
     var zoom = that.map_.getZoom();
 
     if (that.prevZoom_ != zoom) {
       that.prevZoom_ = zoom;
       that.resetViewport();
     }
-  });
 
-  google.maps.event.addListener(this.map_, 'idle', function() {
     that.redraw();
   });
 
@@ -182,24 +167,6 @@ function MarkerClusterer(map, opt_markers, opt_options) {
     this.addMarkers(opt_markers, false);
   }
 }
-
-
-/**
- * The marker cluster image path.
- *
- * @type {string}
- * @private
- */
-MarkerClusterer.prototype.MARKER_CLUSTER_IMAGE_PATH_ = '../images/m';
-
-
-/**
- * The marker cluster image path.
- *
- * @type {string}
- * @private
- */
-MarkerClusterer.prototype.MARKER_CLUSTER_IMAGE_EXTENSION_ = 'png';
 
 
 /**
@@ -809,8 +776,9 @@ function Cluster(markerClusterer) {
   this.center_ = null;
   this.markers_ = [];
   this.bounds_ = null;
-  this.clusterIcon_ = new ClusterIcon(this, markerClusterer.getStyles(),
-      markerClusterer.getGridSize());
+  this.clusterIcon_ = new google.maps.Marker;
+
+  this.clusterIcon_.addListener('click', this.triggerClusterClick.bind(this));
 }
 
 /**
@@ -911,7 +879,7 @@ Cluster.prototype.getBounds = function() {
  * Removes the cluster
  */
 Cluster.prototype.remove = function() {
-  this.clusterIcon_.remove();
+  this.clusterIcon_.setMap(null);
   this.markers_.length = 0;
   delete this.markers_;
 };
@@ -996,272 +964,71 @@ Cluster.prototype.updateIcon = function() {
 
   if (this.markers_.length < this.minClusterSize_) {
     // Min cluster size not yet reached.
-    this.clusterIcon_.hide();
+    this.clusterIcon_.setMap(null);
     return;
   }
 
   var numStyles = this.markerClusterer_.getStyles().length;
   var sums = this.markerClusterer_.getCalculator()(this.markers_, numStyles);
-  this.clusterIcon_.setCenter(this.center_);
-  this.clusterIcon_.setSums(sums);
-  this.clusterIcon_.show();
+  this.clusterIcon_.setPosition(this.center_);
+  this.clusterIcon_.set('icon', createTextInCircleIcon({
+    text: sums.text,
+    fillColor: 'hsl(' + (sums.index * 60) + ', 100%, 50%)' 
+  }));
+  this.clusterIcon_.setMap(this.map_);
 };
-
-
-/**
- * A cluster icon
- *
- * @param {Cluster} cluster The cluster to be associated with.
- * @param {Object} styles An object that has style properties:
- *     'url': (string) The image url.
- *     'height': (number) The image height.
- *     'width': (number) The image width.
- *     'anchor': (Array) The anchor position of the label text.
- *     'textColor': (string) The text color.
- *     'textSize': (number) The text size.
- *     'backgroundPosition: (string) The background postition x, y.
- * @param {number=} opt_padding Optional padding to apply to the cluster icon.
- * @constructor
- * @extends google.maps.OverlayView
- * @ignore
- */
-function ClusterIcon(cluster, styles, opt_padding) {
-  cluster.getMarkerClusterer().extend(ClusterIcon, google.maps.OverlayView);
-
-  this.styles_ = styles;
-  this.padding_ = opt_padding || 0;
-  this.cluster_ = cluster;
-  this.center_ = null;
-  this.map_ = cluster.getMap();
-  this.div_ = null;
-  this.sums_ = null;
-  this.visible_ = false;
-
-  this.setMap(this.map_);
-}
-
 
 /**
  * Triggers the clusterclick event and zoom's if the option is set.
  *
  * @param {google.maps.MouseEvent} event The event to propagate
  */
-ClusterIcon.prototype.triggerClusterClick = function(event) {
-  var markerClusterer = this.cluster_.getMarkerClusterer();
+Cluster.prototype.triggerClusterClick = function(event) {
+  var markerClusterer = this.getMarkerClusterer();
 
   // Trigger the clusterclick event.
-  google.maps.event.trigger(markerClusterer, 'clusterclick', this.cluster_, event);
+  google.maps.event.trigger(markerClusterer, 'clusterclick', this, event);
 
   if (markerClusterer.isZoomOnClick()) {
     // Zoom into the cluster.
-    this.map_.fitBounds(this.cluster_.getBounds());
+    this.map_.fitBounds(this.getBounds());
   }
 };
 
-
-/**
- * Adding the cluster icon to the dom.
- * @ignore
- */
-ClusterIcon.prototype.onAdd = function() {
-  this.div_ = document.createElement('DIV');
-  if (this.visible_) {
-    var pos = this.getPosFromLatLng_(this.center_);
-    this.div_.style.cssText = this.createCss(pos);
-    this.div_.innerHTML = this.sums_.text;
-  }
-
-  var panes = this.getPanes();
-  panes.overlayMouseTarget.appendChild(this.div_);
-
-  var that = this;
-  var isDragging = false;
-  google.maps.event.addDomListener(this.div_, 'click', function(event) {
-    // Only perform click when not preceded by a drag
-    if (!isDragging) {
-      that.triggerClusterClick(event);
-    }
-  });
-  google.maps.event.addDomListener(this.div_, 'mousedown', function() {
-    isDragging = false;
-  });
-  google.maps.event.addDomListener(this.div_, 'mousemove', function() {
-    isDragging = true;
-  });
-};
-
-
-/**
- * Returns the position to place the div dending on the latlng.
- *
- * @param {google.maps.LatLng} latlng The position in latlng.
- * @return {google.maps.Point} The position in pixels.
- * @private
- */
-ClusterIcon.prototype.getPosFromLatLng_ = function(latlng) {
-  var pos = this.getProjection().fromLatLngToDivPixel(latlng);
-
-  if (typeof this.iconAnchor_ === 'object' && this.iconAnchor_.length === 2) {
-    pos.x -= this.iconAnchor_[0];
-    pos.y -= this.iconAnchor_[1];
-  } else {
-    pos.x -= parseInt(this.width_ / 2, 10);
-    pos.y -= parseInt(this.height_ / 2, 10);
-  }
-  return pos;
-};
-
-
-/**
- * Draw the icon.
- * @ignore
- */
-ClusterIcon.prototype.draw = function() {
-  if (this.visible_) {
-    var pos = this.getPosFromLatLng_(this.center_);
-    this.div_.style.top = pos.y + 'px';
-    this.div_.style.left = pos.x + 'px';
-  }
-};
-
-
-/**
- * Hide the icon.
- */
-ClusterIcon.prototype.hide = function() {
-  if (this.div_) {
-    this.div_.style.display = 'none';
-  }
-  this.visible_ = false;
-};
-
-
-/**
- * Position and show the icon.
- */
-ClusterIcon.prototype.show = function() {
-  if (this.div_) {
-    var pos = this.getPosFromLatLng_(this.center_);
-    this.div_.style.cssText = this.createCss(pos);
-    this.div_.style.display = '';
-  }
-  this.visible_ = true;
-};
-
-
-/**
- * Remove the icon from the map
- */
-ClusterIcon.prototype.remove = function() {
-  this.setMap(null);
-};
-
-
-/**
- * Implementation of the onRemove interface.
- * @ignore
- */
-ClusterIcon.prototype.onRemove = function() {
-  if (this.div_ && this.div_.parentNode) {
-    this.hide();
-    this.div_.parentNode.removeChild(this.div_);
-    this.div_ = null;
-  }
-};
-
-
-/**
- * Set the sums of the icon.
- *
- * @param {Object} sums The sums containing:
- *   'text': (string) The text to display in the icon.
- *   'index': (number) The style index of the icon.
- */
-ClusterIcon.prototype.setSums = function(sums) {
-  this.sums_ = sums;
-  this.text_ = sums.text;
-  this.index_ = sums.index;
-  if (this.div_) {
-    this.div_.innerHTML = sums.text;
-  }
-
-  this.useStyle();
-};
-
-
-/**
- * Sets the icon to the the styles.
- */
-ClusterIcon.prototype.useStyle = function() {
-  var index = Math.max(0, this.sums_.index - 1);
-  index = Math.min(this.styles_.length - 1, index);
-  var style = this.styles_[index];
-  this.url_ = style['url'];
-  this.height_ = style['height'];
-  this.width_ = style['width'];
-  this.textColor_ = style['textColor'];
-  this.anchor_ = style['anchor'];
-  this.textSize_ = style['textSize'];
-  this.backgroundPosition_ = style['backgroundPosition'];
-  this.iconAnchor_ = style['iconAnchor'];
-};
-
-
-/**
- * Sets the center of the icon.
- *
- * @param {google.maps.LatLng} center The latlng to set as the center.
- */
-ClusterIcon.prototype.setCenter = function(center) {
-  this.center_ = center;
-};
-
-
-/**
- * Create the css text based on the position of the icon.
- *
- * @param {google.maps.Point} pos The position.
- * @return {string} The css style text.
- */
-ClusterIcon.prototype.createCss = function(pos) {
-  var style = [];
-  style.push('background-image:url(' + this.url_ + ');');
-  var backgroundPosition = this.backgroundPosition_ ? this.backgroundPosition_ : '0 0';
-  style.push('background-position:' + backgroundPosition + ';');
-
-  if (typeof this.anchor_ === 'object') {
-    if (typeof this.anchor_[0] === 'number' && this.anchor_[0] > 0 &&
-        this.anchor_[0] < this.height_) {
-      style.push('height:' + (this.height_ - this.anchor_[0]) +
-          'px; padding-top:' + this.anchor_[0] + 'px;');
-    } else if (typeof this.anchor_[0] === 'number' && this.anchor_[0] < 0 &&
-        -this.anchor_[0] < this.height_) {
-      style.push('height:' + this.height_ + 'px; line-height:' + (this.height_ + this.anchor_[0]) +
-          'px;');
-    } else {
-      style.push('height:' + this.height_ + 'px; line-height:' + this.height_ +
-          'px;');
-    }
-    if (typeof this.anchor_[1] === 'number' && this.anchor_[1] > 0 &&
-        this.anchor_[1] < this.width_) {
-      style.push('width:' + (this.width_ - this.anchor_[1]) +
-          'px; padding-left:' + this.anchor_[1] + 'px;');
-    } else {
-      style.push('width:' + this.width_ + 'px; text-align:center;');
-    }
-  } else {
-    style.push('height:' + this.height_ + 'px; line-height:' +
-        this.height_ + 'px; width:' + this.width_ + 'px; text-align:center;');
-  }
-
-  var txtColor = this.textColor_ ? this.textColor_ : 'black';
-  var txtSize = this.textSize_ ? this.textSize_ : 11;
-
-  style.push('cursor:pointer; top:' + pos.y + 'px; left:' +
-      pos.x + 'px; color:' + txtColor + '; position:absolute; font-size:' +
-      txtSize + 'px; font-family:Arial,sans-serif; font-weight:bold');
-  return style.join('');
-};
+/** Creates an Icon usable by a google.maps.Marker. */
+function createTextInCircleIcon(options) {
+  var canvas = document.createElement('canvas');
+  var context = canvas.getContext("2d");
+  var font = options.font || '14px sans-serif';
+  var radius = options.radius || 14;
+  var offset = options.strokeWeight != null ? options.strokeWeight / 2 : 1;
+  context.font = font;
+  var textWidth = context.measureText(options.text).width;
+  canvas.height = 2 * (radius + offset);
+  canvas.width = 2 * (radius + offset) + Math.max(0, textWidth - radius);
+  var bottom = 2 * radius + offset;
+  var right = canvas.width - offset;
+  context.fillStyle = options.fillColor || 'red';
+  context.strokeStyle = options.strokeColor || 'black';
+  context.lineWidth = 2 * offset;
+  context.beginPath();
+  context.moveTo(radius + offset, offset);
+  context.arcTo(right, offset, right, bottom, radius);
+  context.arcTo(right, bottom, offset, bottom, radius);
+  context.arcTo(offset, bottom, offset, offset, radius);
+  context.arcTo(offset, offset, right, offset, radius);
+  context.fill();
+  context.stroke();
+  context.font = font;
+  context.fillStyle = options.textColor || 'black';
+  context.textAlign = 'center';
+  context.textBaseline = 'middle';
+  context.fillText(options.text, 0.5 * canvas.width, radius + offset);
+  return {
+    url: canvas.toDataURL(),
+    anchor: {x: 0.5 * canvas.width, y: 0.5 * canvas.height}
+  };
+}
 
 
 // Export Symbols for Closure
@@ -1309,7 +1076,3 @@ MarkerClusterer.prototype['draw'] = MarkerClusterer.prototype.draw;
 Cluster.prototype['getCenter'] = Cluster.prototype.getCenter;
 Cluster.prototype['getSize'] = Cluster.prototype.getSize;
 Cluster.prototype['getMarkers'] = Cluster.prototype.getMarkers;
-
-ClusterIcon.prototype['onAdd'] = ClusterIcon.prototype.onAdd;
-ClusterIcon.prototype['draw'] = ClusterIcon.prototype.draw;
-ClusterIcon.prototype['onRemove'] = ClusterIcon.prototype.onRemove;
